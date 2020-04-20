c428c9b8716bab1ae571d93bb8b33952fdb06f0c
Luciano Felix Dias <luciano.felix.555@hotmail.com>
2020-04-20 18:45:26 -0300

    Created Color Saving

--- docs/mixer/script.js
+++ docs/mixer/script.js
@@ -2,16 +2,66 @@ new Vue({
   el: 'main',
   
   data: {
-    red: 15,
-    green: 15,
-    blue: 15
+    color: {
+      red: 15,
+      green: 15,
+      blue: 15
+    },
+    myColors: []
   },
   
+
   computed: {
-    color () {
-      const r = this.toHex(this.red)
-      const g = this.toHex(this.green)
-      const b = this.toHex(this.blue)
+    colorHex () {
+      return this.getColorHex([this.color.red, this.color.green, this.color.blue])
+    },
+
+    colorStyle () {
+      return this.getColorStyle([this.color.red, this.color.green, this.color.blue])
+    },
+
+    emptySlots () {
+      const { length } = this.myColors
+
+      return Math.max(5, 23 - 6 * Math.floor(length / 6)) - length % 6
+    }
+  },
+
+
+  watch: {
+    myColors (val) {
+      window.localStorage.setItem('myColors', val.join('|'))
+    }
+  },
+
+
+  created () {
+    const myColors = window.localStorage.getItem('myColors')
+
+    if (myColors)
+      this.myColors = myColors.split(/\|/g).map(a => a.split(/\,/g).map(a => parseInt(a)))
+
+    else window.localStorage.setItem('myColors', this.myColors)
+  },
+  
+
+  methods: {
+    getColorStyle (color) {
+      const reduce = color.reduce((a, b) => a + b)
+      const isDark = reduce < 18
+      const isDarken =  reduce < 9
+
+      return {
+        '--color': this.getColorHex(color).mix,
+        color: isDark ? '#FFF' : '#000',
+        borderColor: isDarken ? '#444' : 'transparent'
+      }
+    },
+
+    getColorHex (color) {
+      const r = this.numToHex(color[0])
+      const g = this.numToHex(color[1])
+      const b = this.numToHex(color[2])
       
       return {
         red: `#${r}00`,
@@ -20,10 +70,36 @@ new Vue({
         mix: `#${r}${g}${b}`
       }
     },
-  },
-  
-  methods: {
-    toHex (num) {
+
+    randomize () {
+      (function randomize (count = 0) {
+        this.color = {
+          red: this.rand(0, 15),
+          green: this.rand(0, 15),
+          blue: this.rand(0, 15)
+        }
+
+        if (count < 8)
+          requestAnimationFrame(() => randomize.call(this, count + 1))
+      }).call(this)
+    },
+
+    setColor ([ red, green, blue ]) {
+      this.color = { red, green, blue }
+
+      document.querySelector('main').scrollTo({
+        left: 0,
+        behavior: 'smooth'
+      })
+    },
+    saveColor () {
+      this.myColors.push([this.color.red, this.color.green, this.color.blue])
+    },
+    removeColor (index) {
+      this.$delete(this.myColors, index)
+    },
+
+    numToHex (num) {
       return num.toString(16).toUpperCase()
     },
     lim (val, min, max) {
@@ -31,21 +107,6 @@ new Vue({
     },
     rand (min, max) {
       return Math.round(Math.random() * (max - min) + min)
-    },
-    randomize () {
-      let count = 0
-
-      const randomize = () => {
-        count++
-        
-        this.red = this.rand(0, 15)
-        this.green = this.rand(0, 15)
-        this.blue = this.rand(0, 15)
-
-        if (count < 10)
-          requestAnimationFrame(randomize)
-      }
-      randomize()
     }
   },
 
@@ -59,34 +120,43 @@ new Vue({
 
         hammertime.get('pan').set({ threshold: 0 });
 
-        hammertime.on('panstart', event => {
-          oldVal = key === 'light' ? {
-            red: $vue.red,
-            green: $vue.green,
-            blue: $vue.blue
-          } : $vue[key]
-        })
+        hammertime.on('panstart', () => oldVal = Object.assign({}, $vue.color))
 
         hammertime.on('pan', (event) => {
           event.preventDefault()
           
-          const val = Math.round($vue.lim(event.deltaY / 15, -15, 15))
+          const val = Math.round($vue.lim(event.deltaY / 20, -15, 15))
 
           const newVal = key === 'light' ? {
             red: $vue.lim(oldVal.red - val, 0, 15),
             green: $vue.lim(oldVal.green - val, 0, 15),
             blue: $vue.lim(oldVal.blue - val, 0, 15)
-          } : $vue.lim(oldVal - val, 0, 15)
+          } : $vue.lim(oldVal[key] - val, 0, 15)
           
-          if (key === 'light') {
-            $vue.$set($vue, 'red', newVal.red)
-            $vue.$set($vue, 'green', newVal.green)
-            $vue.$set($vue, 'blue', newVal.blue)
-          }
-          else $vue.$set($vue, key, newVal)
+          if (key === 'light') $vue.$set($vue, 'color', newVal)
+          else $vue.$set($vue.color, key, newVal)
         })
 
         el.addEventListener('touchstart', event => event.preventDefault())
+      }
+    },
+
+    press: {
+      bind (el, binding, vnode) {
+        const hammertime = new Hammer(el)
+
+        function emit (vnode, name, data) {
+          const handlers = (vnode.data && vnode.data.on) || (vnode.componentOptions && vnode.componentOptions.listeners)
+
+          if (handlers && handlers[name])
+            handlers[name].fns(data)
+        }
+
+        hammertime.get('press').set({ time: 450 });
+
+        hammertime.on('pressup', (event) => {
+          emit(vnode, 'press', event)
+        })
       }
     }
   }
