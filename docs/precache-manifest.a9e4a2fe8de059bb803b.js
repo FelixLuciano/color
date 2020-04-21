62a5f4f220fb1f7086d0208c89d1bb21d571f71d
Luciano Felix Dias <luciano.felix.555@hotmail.com>
2020-04-21 18:54:06 -0300

    Tip Service-worker URLs

--- docs/precache-manifest.a9e4a2fe8de059bb803b.js
+++ docs/precache-manifest.a9e4a2fe8de059bb803b.js
@@ -1,7 +1,7 @@
 self.__precacheManifest = [
-  { "revision": "a9e4a2fe8de059bb803b", "url": "/tri/index.html" },
-  { "revision": "a9e4a2fe8de059bb803b", "url": "/tri/mixer/index.html" },
-  { "revision": "a9e4a2fe8de059bb803b", "url": "/tri/style.css" },
-  { "revision": "a9e4a2fe8de059bb803b", "url": "/tri/mixer/style.css" },
-  { "revision": "a9e4a2fe8de059bb803b", "url": "/tri/mixer/script.js" }
+  { "revision": "a9e4a2fe8de059bb803b", "url": "/tri/docs/index.html" },
+  { "revision": "a9e4a2fe8de059bb803b", "url": "/tri/docs/mixer/index.html" },
+  { "revision": "a9e4a2fe8de059bb803b", "url": "/tri/docs/style.css" },
+  { "revision": "a9e4a2fe8de059bb803b", "url": "/tri/docs/mixer/style.css" },
+  { "revision": "a9e4a2fe8de059bb803b", "url": "/tri/docs/mixer/script.js" }
 ]
