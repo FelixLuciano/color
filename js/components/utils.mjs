6a7b4b55459590885eba1d8ae555f42aa09b95ba
FelixLuciano <felix.lucianodias@gmail.com>
2021-06-17 17:22:47 -0300

    Contrast border, Banner animation & Vectors split

--- js/components/utils.mjs
+++ js/components/utils.mjs
@@ -2,8 +2,12 @@ export function clamp(value, min, max) {
 	return Math.min(Math.max(value, min), max)
 }
 
+export function num2hex(num) {
+	return num.toString(16).toUpperCase()
+}
+
 export function getHexadecimals(color) {
-	const [r, g, b] = color.map((num) => num.toString(16).toUpperCase())
+	const [r, g, b] = color.map(num2hex)
 
 	return {
 		red: `#${r}00`,
@@ -14,13 +18,17 @@ export function getHexadecimals(color) {
 }
 
 export function getDisplay(color) {
-	const reduce = color.reduce((a, b) => a + b)
-	const isDark = reduce < 18
-	const isDarken = reduce < 9
+	const max =  Math.max(...color)
+	const isDark = max < 10
+	const isDarken = max < 8
+	const alpha = Math.round(7 * (1 - max / 15))
+
+	const textColor = isDark ? "#FFF" : "#000"
+	const borderColor = isDarken ? `#FFF${ num2hex(alpha) }` : "transparent"
 
 	return {
 		"--color": getHexadecimals(color).color,
-		color: isDark ? "#FFF" : "#000",
-		borderColor: isDarken ? "#444" : "transparent"
+		color: textColor,
+		borderColor: borderColor
 	}
 }
