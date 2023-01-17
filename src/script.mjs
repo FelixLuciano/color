03bc5c726412d7a66990b7d03b16f4af386c26c4
FelixLuciano <felix.lucianodias@gmail.com>
2023-01-17 20:28:09 -0300

    feat: color immutability and edge cases

--- src/script.mjs
+++ src/script.mjs
@@ -3,248 +3,239 @@ import persist from '@alpinejs/persist'
 
 
 class Color {
-    constructor(r = 1, g = 1, b = 1) {
-        this.state = {
-            rgb: [r, g, b],
-            hex: null,
-            hue: null,
-            chroma: null,
-            light: null,
-        }
-        this.transition = null
+  constructor(r = 1, g = 1, b = 1) {
+    if (r < 0 || r > 1) throw new Error('Red value is out of range')
+    if (g < 0 || g > 1) throw new Error('Green value is out of range')
+    if (b < 0 || b > 1) throw new Error('Blue value is out of range')
 
-        this.updateHsvAndHexFromRgb()
-    }
+    const vec = [this.red, this.green, this.blue] = [r, g, b]
+    const cmax = Math.max(r, g, b)
+    const cmin = Math.min(r, g, b)
+    const delta = cmax - cmin
+    this.hex = '#' + vec.map(value => Math.round(value * 15).toString(16).toUpperCase()).join('')
+    this.hue = 60 * (
+      delta === 0 ? 0 :
+      cmax === r ? (g - b) / delta + (g < b ? 6 : 0) :
+      cmax === g ? (b - r) / delta + 2 :
+      cmax === b ? (r - g) / delta + 4 :
+      null
+    )
+    this.chroma = cmax === 0 ? 0 : delta / cmax
+    this.light = cmax
+    this.transition = null
+  }
 
-    get vector() {
-        return this.state.rgb
-    }
-    set vector(values) {
-        if (values.length != 3)
-            throw new Error(`Cannot set vector to ${values} as it does not have 3 values`)
+  static fromHex(hex) {
+    if (!hex.match(/^#([\da-f]{3}){1,2}$/i))
+      throw new Error(`${hex} is not a valid hexadecimal color code.`)
 
-        const dR = (values[0] - this.state.rgb[0]) / 10
-        const dG = (values[1] - this.state.rgb[1]) / 10
-        const dB = (values[2] - this.state.rgb[2]) / 10
-        const round = (num) => Math.round((num + Number.EPSILON) * 1_000) / 1_000
+    const size = hex.length === 7 ? 2 : 1
+    const max = hex.length === 7 ? 255 : 15
+    const matches = hex.match(new RegExp(`[\\da-f]{${size}}`, 'gi'))
 
-        const transition = (i) => {
-            this.state.rgb[0] = round(this.state.rgb[0] + dR)
-            this.state.rgb[1] = round(this.state.rgb[1] + dG)
-            this.state.rgb[2] = round(this.state.rgb[2] + dB)
+    if (!matches) throw new Error('Invalid hex code')
 
-            if (i < 9)
-                this.transition = requestAnimationFrame(() => transition(i + 1))
-            else
-                this.state.rgb = values
+    const [r, g, b] = matches.map((match) => parseInt(match, 16) / max)
 
-            this.updateHsvAndHexFromRgb()
-        }
+    return new Color(r, g, b)
+  }
 
-        cancelAnimationFrame(this.transition)
-        transition(0)
-    }
+  static fromHsv(h, s, v) {
+    if (s < 0 || s > 1) throw new Error(`Saturation value {s} is out of range`)
+    if (v < 0 || v > 1) throw new Error(`Value {v} is out of range`)
 
-    get red() {
-        return this.state.rgb[0]
-    }
-    set red(value) {
-        this.state.rgb[0] = parseFloat(value)
+    const c = s * v
+    const x = c * (1 - Math.abs(h / 60 % 2 - 1))
+    const m = v - c
+    let rgb = (
+      h >= 360 || h < 60 ? [c, x, 0] :
+      h >= 60 && h < 120 ? [x, c, 0] :
+      h >= 120 && h < 180 ? [0, c, x] :
+      h >= 180 && h < 240 ? [0, x, c] :
+      h >= 240 && h < 300 ? [x, 0, c] :
+      h >= 300 && h < 360 ? [c, 0, x] :
+      null
+    ).map(c => c + m)
+    const color = new Color(...rgb)
 
-        this.updateHsvAndHexFromRgb()
-    }
+    // Keeps original parameters
+    color.hue = h
+    color.chroma = s
+    color.light = v
 
-    get green() {
-        return this.state.rgb[1]
-    }
-    set green(value) {
-        this.state.rgb[1] = parseFloat(value)
-
-        this.updateHsvAndHexFromRgb()
-    }
-
-    get blue() {
-        return this.state.rgb[2]
-    }
-    set blue(value) {
-        this.state.rgb[2] = parseFloat(value)
-
-        this.updateHsvAndHexFromRgb()
-    }
-
-    get hex() {
-        return this.state.hex
-    }
-    set hex(hex) {
-        this.state = Color.fromHex(hex).state
-    }
-
-    get hue() {
-        return this.state.hue
-    }
-    set hue(value) {
-        this.setHSv(parseInt(value), this.state.chroma, this.state.light)
-    }
-
-    get chroma() {
-        return this.state.chroma
-    }
-    set chroma(value) {
-        this.setHSv(this.state.hue, parseFloat(value), this.state.light)
-    }
-
-    get light() {
-        return this.state.light
-    }
-    set light(value) {
-        this.setHSv(this.state.hue, this.state.chroma, parseFloat(value))
-    }
-
-    updateHsvAndHexFromRgb() {
-        const [r, g, b] = this.vector
-        const cmax = Math.max(r, g, b)
-        const cmin = Math.min(r, g, b)
-        const delta = (cmax - cmin)
-
-        let h
-        if (delta === 0) h = this.state.hue / 60
-        else if (cmax === r) h = (g - b) / delta + (g < b ? 6 : 0)
-        else if (cmax === g) h = (b - r) / delta + 2
-        else if (cmax === b) h = (r - g) / delta + 4
-
-        if (h == 0 && this.state.hue > 240) h = 6
-
-        this.state.hue = h * 60
-        this.state.chroma = cmax == 0 ? 0 : delta / cmax
-        this.state.light = cmax
-        this.state.hex = '#' + this.vector.map(value => parseInt(value * 15).toString(16).toUpperCase()).join('')
-    }
-
-    setHSv(h, s, l) {
-        this.state = Color.fromHsv(h, s, l).state
-    }
-
-    static fromHex(hex) {
-        const size = hex.length === 7 ? 2 : 1
-        const matches = hex.match(new RegExp(`[\\da-f]{${size}}`, 'gi'))
-        const base = hex.length === 7 ? 256 : 15
-
-        if (!matches || matches.length !== 3) throw new Error('Invalid hex code')
-
-        const [r, g, b] = matches.map((match) => parseInt(match, 16) / base)
-
-        return new Color(r, g, b)
-    }
-
-    static fromHsv(h, s, b) {
-        // Check for invalid values of chroma and light
-        if (s < 0 || s > 1) throw new Error("Invalid chroma")
-        if (b < 0 || b > 1) throw new Error("Invalid light")
-
-        // Calculate the chroma, hue shift, and the secondary color
-        const c = s * b
-        const h_ = h / 60
-        const x = c * (1- Math.abs(h_ % 2 - 1))
-        const m = b - c
-
-        // Calculate the RGB values for the color
-        let rgb = (
-            h_ >= 6 || h_ < 1 ? [c, x, 0] :
-            h_ >= 1 && h_ < 2 ? [x, c, 0] :
-            h_ >= 2 && h_ < 3 ? [0, c, x] :
-            h_ >= 3 && h_ < 4 ? [0, x, c] :
-            h_ >= 4 && h_ < 5 ? [x, 0, c] :
-            h_ >= 5 && h_ < 6 ? [c, 0, x] : undefined
-        ).map(c => c + m)
-
-        // Create the color object and set the HSv values
-        const color = new Color(...rgb)
-        color.state.hue = h
-        color.state.chroma = s
-        color.state.light = b
-
-        return color
-    }
+    return color
+  }
 }
 
 
 function picker() {
-    function getRandomColor() {
-        return new Color(Math.random(), Math.random(), Math.random())
-    }
+  function getRandomColor() {
+    return new Color(Math.random(), Math.random(), Math.random())
+  }
 
-    return {
-        hex: '#FFF',
-        color: new Color(),
-        randomColor: null,
-        $storage: this.$persist([]).as('com.lucianofelix.tri.storage'),
+  return {
+    hexInput: '#FFF',
+    color: new Color(),
+    randomColor: null,
+    $storage: this.$persist([]).as('com.lucianofelix.tri.storage'),
 
-        get hex() {
-            return this.hex
-        },
-        set hex(value) {
-            this.hex = value.toUpperCase()
+    get hex() {
+      return this.hexInput
+    },
+    set hex(value) {
+      if (!value.match(/^#[\da-f]{0,6}$/i))
+        return
 
-            try {
-                this.color.hex = value
-            }
-            catch (error) {
-                console.warn(error)
-            }
-        },
-        get red() {
-            return new Color(this.color.red, 0, 0)
-        },
-        get green() {
-            return new Color(0, this.color.green, 0)
-        },
-        get blue() {
-            return new Color(0, 0, this.color.blue)
-        },
-        get hue() {
-            return Color.fromHsv(this.color.hue, 1, 1)
-        },
-        get chroma() {
-            return Color.fromHsv(this.color.hue, this.color.chroma, .5 + this.color.chroma / 2)
-        },
-        get light() {
-            return Color.fromHsv(0, 0, this.color.light)
-        },
-        get complementary() {
-            const hue = (this.color.hue + 180 + 43) % 360
+      this.hexInput = value.toUpperCase()
 
-            return Color.fromHsv(hue, this.color.chroma, this.color.light)
-        },
-        get complementary2() {
-            const hue = (this.color.hue + 180 - 43) % 360
+      try {
+        this.color = Color.fromHex(value)
+        this.hexInput = this.color.hex
+      }
+      catch (error) {
+        console.warn(error)
+      }
+    },
+    get red() {
+      return this.color.red
+    },
+    set red(value) {
+      const oldHue = this.hue
 
-            return Color.fromHsv(hue, this.color.chroma, this.color.light)
-        },
-        get storage() {
-            return this.$storage.map(item => Color.fromHex(item))
-        },
+      this.setColor(new Color(value, this.green, this.blue))
 
-        selectColor(color) {
-            this.color.vector = color.vector
+      // keeps continuity when cycling from blue back to red
+      if (this.hue == 0 && oldHue > 240) this.color.hue = 360
+    },
+    get redDisplay() {
+      return new Color(this.red, 0, 0)
+    },
+    get green() {
+      return this.color.green
+    },
+    set green(value) {
+      this.setColor(new Color(this.red, value, this.blue))
+    },
+    get greenDisplay() {
+      return new Color(0, this.green, 0)
+    },
 
-            this.$refs.color.scrollIntoView({
-                block: 'nearest'
-            })
-        },
-        selectRandomColor() {
-            this.selectColor(this.randomColor)
+    get blue() {
+      return this.color.blue
+    },
+    set blue(value) {
+      const oldHue = this.hue
 
-            this.randomColor = getRandomColor()
-        },
-        store(color) {
-            if(!this.$storage.includes(color.hex))
-                this.$storage.unshift(color.hex)
-        },
+      this.setColor(new Color(this.red, this.green, value))
 
-        init() {
-            this.randomColor = getRandomColor()
-        },
-    }
+      // keeps continuity when cycling from blue back to red
+      if (this.color.hue == 0 && oldHue > 240) this.color.hue = 360
+    },
+    get blueDisplay() {
+      return new Color(0, 0, this.blue)
+    },
+
+    get hue() {
+      return this.color.hue
+    },
+    set hue(value) {
+      this.color = Color.fromHsv(value, this.chroma, this.light)
+      this.hexInput = this.color.hex
+    },
+    get hueDisplay() {
+      return Color.fromHsv(this.hue, 1, 1)
+    },
+
+    get chroma() {
+      return this.color.chroma
+    },
+    set chroma(value) {
+      this.color = Color.fromHsv(this.hue, value, this.light)
+      this.hexInput = this.color.hex
+    },
+    get chromaDisplay() {
+      return Color.fromHsv(this.hue, this.chroma, .5 + this.chroma / 2)
+    },
+
+    get light() {
+      return this.color.light
+    },
+    set light(value) {
+      this.color = Color.fromHsv(this.hue, this.chroma, value)
+      this.hexInput = this.color.hex
+    },
+    get lightDisplay() {
+      return Color.fromHsv(0, 0, this.light)
+    },
+
+    get complementary() {
+      const hue = (this.color.hue + 180 + 43) % 360
+
+      return Color.fromHsv(hue, this.color.chroma, this.color.light)
+    },
+    get complementary2() {
+      const hue = (this.color.hue + 180 - 43) % 360
+
+      return Color.fromHsv(hue, this.color.chroma, this.color.light)
+    },
+    get storage() {
+      return this.$storage.map(item => Color.fromHex(item))
+    },
+
+    setColor(color) {
+      const oldHue = this.hue
+      const oldChroma = this.chroma
+
+      this.color = color
+      this.hexInput = this.color.hex
+
+      // keeps continuity when set to 0
+      if (this.light === 0) {
+        this.hue = oldHue
+        this.chroma = oldChroma
+      }
+    },
+    selectColor(color) {
+      if (!(color instanceof Color))
+        throw new Error(`Object is not a instance of Color`)
+
+      const dR = (color.red - this.color.red) / 10
+      const dG = (color.green - this.color.green) / 10
+      const dB = (color.blue - this.color.blue) / 10
+      const round = (num) => Math.round((num + Number.EPSILON) * 1_000) / 1_000
+      const transition = (step) => {
+        if (step >= 0) {
+          const r = round(color.red - dR * step)
+          const g = round(color.green - dG * step)
+          const b = round(color.blue - dB * step)
+
+          this.setColor(new Color(r, g, b))
+          this.transition = requestAnimationFrame(() => transition(step - 1))
+        }
+        else
+          this.color = color
+      }
+
+      this.$refs.color.scrollIntoView({ block: 'nearest' })
+      cancelAnimationFrame(this.transition)
+      transition(10)
+    },
+    selectRandomColor() {
+      this.selectColor(this.randomColor)
+
+      this.randomColor = getRandomColor()
+    },
+
+    store(color) {
+      if (!this.$storage.includes(color.hex))
+        this.$storage.unshift(color.hex)
+    },
+
+    init() {
+      this.color.hue = 60
+      this.randomColor = getRandomColor()
+    },
+  }
 }
 
 
