cc10ad56114406ac05b883f51c9ee79563882603
FelixLuciano <felix.lucianodias@gmail.com>
2023-02-19 10:45:08 -0300

    refactor: code cleaned

--- src/script.mjs
+++ src/script.mjs
@@ -2,111 +2,15 @@ import Alpine from 'alpinejs'
 import persist from '@alpinejs/persist'
 
 
-class Color {
-  constructor(r = 1, g = 1, b = 1) {
-    if (r < 0 || r > 1) throw new Error('Red value is out of range')
-    if (g < 0 || g > 1) throw new Error('Green value is out of range')
-    if (b < 0 || b > 1) throw new Error('Blue value is out of range')
-
-    const vec = [this.red, this.green, this.blue] = [r, g, b]
-    const sRGBVec = vec.map((n) => n <= 0.03928 ? n / 12.92 : ((n + 0.055) / 1.055) ** 2.4)
-    const cmax = Math.max(r, g, b)
-    const cmin = Math.min(r, g, b)
-    const delta = cmax - cmin
-    this.hex = '#' + vec.map(value => Math.round(value * 15).toString(16).toUpperCase()).join('')
-    this.hue = 60 * (
-      delta === 0 ? 0 :
-        cmax === r ? (g - b) / delta + (g < b ? 6 : 0) :
-          cmax === g ? (b - r) / delta + 2 :
-            cmax === b ? (r - g) / delta + 4 :
-              null
-    )
-    this.chroma = cmax === 0 ? 0 : delta / cmax
-    this.light = cmax
-    this.transition = null
-    this.luminance = 0.2126 * sRGBVec[0] + 0.7152 * sRGBVec[1] + 0.0722 * sRGBVec[2]
-  }
-
-  static fromHex(hex) {
-    if (!hex.match(/^#([\da-f]{3}){1,2}$/i))
-      throw new Error(`${hex} is not a valid hexadecimal color code.`)
-
-    const size = hex.length === 7 ? 2 : 1
-    const max = hex.length === 7 ? 255 : 15
-    const matches = hex.match(new RegExp(`[\\da-f]{${size}}`, 'gi'))
-
-    if (!matches) throw new Error('Invalid hex code')
-
-    const [r, g, b] = matches.map((match) => parseInt(match, 16) / max)
-
-    return new Color(r, g, b)
-  }
-
-  static fromHsv(h, s, v) {
-    if (s < 0 || s > 1) throw new Error(`Saturation value {s} is out of range`)
-    if (v < 0 || v > 1) throw new Error(`Value {v} is out of range`)
-
-    const c = s * v
-    const x = c * (1 - Math.abs(h / 60 % 2 - 1))
-    const m = v - c
-    let rgb = (
-      h >= 360 || h < 60 ? [c, x, 0] :
-        h >= 60 && h < 120 ? [x, c, 0] :
-          h >= 120 && h < 180 ? [0, c, x] :
-            h >= 180 && h < 240 ? [0, x, c] :
-              h >= 240 && h < 300 ? [x, 0, c] :
-                h >= 300 && h < 360 ? [c, 0, x] :
-                  null
-    ).map(c => c + m)
-    const color = new Color(...rgb.map(n => Math.round(n * 15) / 15))
-
-    // Keeps original parameters
-    color.hue = h
-    color.chroma = s
-    color.light = v
-
-    return color
-  }
-
-  copy() {
-    return new Color(this.red, this.green, this.blue)
-  }
-
-  transform(matrix) {
-    const r = matrix[0][0] * this.red + matrix[0][1] * this.green + matrix[0][2] * this.blue
-    const g = matrix[1][0] * this.red + matrix[1][1] * this.green + matrix[1][2] * this.blue
-    const b = matrix[2][0] * this.red + matrix[2][1] * this.green + matrix[2][2] * this.blue
-
-    return new Color(r, g, b)
-  }
-
-  getContrast(color) {
-    const l1 = this.luminance + 0.05
-    const l2 = color.luminance + 0.05
-
-    return l1 > l2 ? l1 / l2 : l2 / l1
-  }
-}
-
-
 function picker() {
-  function getRandomColor() {
-    return new Color(Math.random(), Math.random(), Math.random())
-  }
+  const BLACK = new Color(0, 0, 0)
+  const WHITE = new Color(1, 1, 1)
 
   return {
+    color: new Color(1, 1, 1),
+
     hexInput: '#FFF',
     inputTimeout: null,
-    color: new Color(),
-    randomColor: null,
-    background: Color.fromHex('#CCC'),
-    foreground: Color.fromHex('#333'),
-    $storage: this.$persist([]).as('com.lucianofelix.tri.storage'),
-    confirmDelete: false,
-
-    BLACK: new Color(0, 0, 0),
-    WHITE: new Color(1, 1, 1),
-
     get hex() {
       return this.hexInput
     },
@@ -127,6 +31,7 @@ function picker() {
         }
       }, 512)
     },
+
     get red() {
       return this.color.red
     },
@@ -141,6 +46,7 @@ function picker() {
     get redDisplay() {
       return new Color(this.red, 0, 0)
     },
+
     get green() {
       return this.color.green
     },
@@ -149,6 +55,9 @@ function picker() {
     },
     get greenDisplay() {
       return new Color(0, this.green, 0)
+    },
+    get greenDisplayBind() {
+      return this.getDisplayBind('greenDisplay')
     },
 
     get blue() {
@@ -210,6 +119,13 @@ function picker() {
       return Color.fromHsv(hue, this.color.chroma, this.color.light)
     },
 
+    randomColor: null,
+    selectRandomColor() {
+      this.selectColor(this.randomColor)
+
+      this.randomColor = getRandomColor()
+    },
+
     get deuteranopia() {
       return this.color.transform([
         [0.625, 0.375, 0.000],
@@ -245,7 +161,9 @@ function picker() {
         [0.299, 0.587, 0.114],
       ])
     },
-
+    
+    background: Color.fromHex('#CCC'),
+    foreground: Color.fromHex('#333'),
     setBackgroundColor() {
       this.background = this.color.copy()
     },
@@ -274,16 +192,24 @@ function picker() {
       else return Color.fromHex('#D22')
     },
 
-    get storage() {
-      return this.$storage.map(item => Color.fromHex(item))
-    },
-
-    getDisplayBind(color) {
+    getDisplayBind(name) {
       return {
-        ':class': `{'color--dark': ${color}.getContrast(WHITE) > ${color}.getContrast(BLACK), 'color--darken': ${color}.getContrast(WHITE) > 18}`,
-        ':style': `{'--color': ${color}.hex}`,
+        ':class': `getDisplayClass(${name})`,
+        ':style': `getDisplayStyle(${name})`,
       }
     },
+    getDisplayClass(color) {
+      return {
+        'color--dark': color.getContrast(WHITE) > color.getContrast(BLACK),
+        'color--darken': color.light < .25,
+      }
+    },
+    getDisplayStyle(color) {
+      return {
+        '--color': color.hex
+      }
+    },
+
     setColor(color) {
       const oldHue = this.hue
       const oldChroma = this.chroma
@@ -331,12 +257,12 @@ function picker() {
       cancelAnimationFrame(this.transition)
       transition()
     },
-    selectRandomColor() {
-      this.selectColor(this.randomColor)
 
-      this.randomColor = getRandomColor()
+    $storage: this.$persist([]).as('com.lucianofelix.tri.storage'),
+    confirmDelete: false,
+    get storage() {
+      return this.$storage.map(item => Color.fromHex(item))
     },
-
     store(color) {
       if (!this.$storage.includes(color.hex))
         this.$storage.unshift(color.hex)
@@ -360,11 +286,103 @@ function picker() {
       this.randomColor = getRandomColor()
     },
   }
+
+  function getRandomColor() {
+    return new Color(Math.random(), Math.random(), Math.random())
+  }
+}
+
+
+class Color {
+  constructor(r, g, b) {
+    if (r < 0 || r > 1) throw new Error('Red value is out of range')
+    if (g < 0 || g > 1) throw new Error('Green value is out of range')
+    if (b < 0 || b > 1) throw new Error('Blue value is out of range')
+
+    const vec = [this.red, this.green, this.blue] = [r, g, b]
+    const sRGBVec = vec.map((n) => n <= 0.03928 ? n / 12.92 : ((n + 0.055) / 1.055) ** 2.4)
+    const cmax = Math.max(r, g, b)
+    const cmin = Math.min(r, g, b)
+    const delta = cmax - cmin
+    this.hex = '#' + vec.map(value => Math.round(value * 15).toString(16).toUpperCase()).join('')
+    this.hue = 60 * (
+      delta === 0 ? 0 :
+      cmax  === r ? (g - b) / delta + (g < b ? 6 : 0) :
+      cmax  === g ? (b - r) / delta + 2 :
+      cmax  === b ? (r - g) / delta + 4 :
+      null
+    )
+    this.chroma = cmax === 0 ? 0 : delta / cmax
+    this.light = cmax
+    this.transition = null
+    this.luminance = 0.2126 * sRGBVec[0] + 0.7152 * sRGBVec[1] + 0.0722 * sRGBVec[2]
+  }
+
+  static fromHex(hex) {
+    if (!hex.match(/^#([\da-f]{3}){1,2}$/i))
+      throw new Error(`${hex} is not a valid hexadecimal color code.`)
+
+    const size = hex.length === 7 ? 2 : 1
+    const max = hex.length === 7 ? 255 : 15
+    const matches = hex.match(new RegExp(`[\\da-f]{${size}}`, 'gi'))
+
+    if (!matches) throw new Error('Invalid hex code')
+
+    const [r, g, b] = matches.map((match) => parseInt(match, 16) / max)
+
+    return new Color(r, g, b)
+  }
+
+  static fromHsv(h, s, v) {
+    if (s < 0 || s > 1) throw new Error(`Saturation value {s} is out of range`)
+    if (v < 0 || v > 1) throw new Error(`Value {v} is out of range`)
+
+    const c = s * v
+    const x = c * (1 - Math.abs(h / 60 % 2 - 1))
+    const m = v - c
+    let rgb = (
+      h >= 360 || h <  60 ? [c, x, 0] :
+      h >=  60 && h < 120 ? [x, c, 0] :
+      h >= 120 && h < 180 ? [0, c, x] :
+      h >= 180 && h < 240 ? [0, x, c] :
+      h >= 240 && h < 300 ? [x, 0, c] :
+      h >= 300 && h < 360 ? [c, 0, x] :
+      null
+    ).map(c => c + m)
+    const color = new Color(...rgb.map(n => Math.round(n * 15) / 15))
+
+    // Keeps original parameters
+    color.hue = h
+    color.chroma = s
+    color.light = v
+
+    return color
+  }
+
+  copy() {
+    return new Color(this.red, this.green, this.blue)
+  }
+
+  transform(matrix) {
+    if (matrix.length != 3 || matrix.some(row => row.length != 3)) throw new Error('Invalid matrix size. use 3 by 3')
+
+    const vec = [this.red, this.green, this.blue]
+    const [r, g, b] = matrix.map((row) => {
+      return row.reduce((acc, curr, i) => acc + curr * vec[i], 0)
+    })
+
+    return new Color(r, g, b)
+  }
+
+  getContrast(color) {
+    const l1 = this.luminance + 0.05
+    const l2 = color.luminance + 0.05
+
+    return l1 > l2 ? l1 / l2 : l2 / l1
+  }
 }
 
 
 Alpine.plugin(persist)
 Alpine.data('picker', picker)
 Alpine.start()
-
-document.querySelector('#logo').addEventListener('click', event => event.preventDefault())
