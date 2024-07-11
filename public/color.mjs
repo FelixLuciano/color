7cc52af10a4268f24bfa4cef32c1669219ceed99
FelixLuciano <felix.lucianodias@gmail.com>
2024-07-10 22:18:23 -0300

    refactor: Optimize Color class and update dependencies

--- public/color.mjs
+++ public/color.mjs
@@ -1,59 +1,29 @@
+import * as conversions from '/conversions.mjs'
+
+const vecDot = (vec1, vec2) => vec1.reduce((acc, factor, i) => acc + factor * vec2[i], 0)
+const matMul = (vec, mat) => mat.map((row) => vecDot(row, vec))
+
 export default class Color {
   constructor(r, g, b) {
-    if (r < 0 || r > 1) throw new Error('Red value is out of range')
-    if (g < 0 || g > 1) throw new Error('Green value is out of range')
-    if (b < 0 || b > 1) throw new Error('Blue value is out of range')
-
-    const vec = [this.red, this.green, this.blue] = [r, g, b]
-    const sRGBVec = vec.map((n) => n <= 0.03928 ? n / 12.92 : ((n + 0.055) / 1.055) ** 2.4)
-    const cmax = Math.max(r, g, b)
-    const cmin = Math.min(r, g, b)
-    const delta = cmax - cmin
-    this.hex = '#' + vec.map(value => Math.round(value * 255).toString(16).toUpperCase().padStart(2, '0')).join('')
-    this.hue = 60 * (
-      delta === 0 ? 0 :
-      cmax  === r ? (g - b) / delta + (g < b ? 6 : 0) :
-      cmax  === g ? (b - r) / delta + 2 :
-      cmax  === b ? (r - g) / delta + 4 :
-      null
-    )
-    this.chroma = cmax === 0 ? 0 : delta / cmax
-    this.light = cmax
-    this.luminance = 0.2126 * sRGBVec[0] + 0.7152 * sRGBVec[1] + 0.0722 * sRGBVec[2]
+    const [h, s, v] = conversions.srgb_to_okhsv(r * 255, g * 255, b * 255)
+  
+    this.vec = [this.red, this.green, this.blue] = [r, g, b]
+    this.hex = conversions.rgb_to_hex(r, g, b).toUpperCase()
+    this.hue = h
+    this.chroma = isNaN(s) ? 0 : s
+    this.light = isNaN(v) ? 0 : v
+    this.luminance = Math.pow(conversions.srgb_to_okhsl(r, g, b)[2], 3)
   }
 
   static fromHex(hex) {
-    if (!hex.match(/^#([\da-f]{3}){1,2}$/i))
-      throw new Error(`${hex} is not a valid hexadecimal color code.`)
-
-    const size = hex.length === 7 ? 2 : 1
-    const max = hex.length === 7 ? 255 : 15
-    const matches = hex.match(new RegExp(`[\\da-f]{${size}}`, 'gi'))
-
-    if (!matches) throw new Error('Invalid hex code')
-
-    const [r, g, b] = matches.map((match) => parseInt(match, 16) / max)
+    const [r, g, b] = conversions.hex_to_rgb(hex).map((n) => n / 255)
 
     return new Color(r, g, b)
   }
 
   static fromHsv(h, s, v) {
-    if (s < 0 || s > 1) throw new Error(`Saturation value {s} is out of range`)
-    if (v < 0 || v > 1) throw new Error(`Value {v} is out of range`)
-
-    const c = s * v
-    const x = c * (1 - Math.abs(h / 60 % 2 - 1))
-    const m = v - c
-    let rgb = (
-      h >= 360 || h <  60 ? [c, x, 0] :
-      h >=  60 && h < 120 ? [x, c, 0] :
-      h >= 120 && h < 180 ? [0, c, x] :
-      h >= 180 && h < 240 ? [0, x, c] :
-      h >= 240 && h < 300 ? [x, 0, c] :
-      h >= 300 && h < 360 ? [c, 0, x] :
-      null
-    ).map(c => c + m)
-    const color = new Color(...rgb.map(n => Math.round(n * 255) / 255))
+    const [r, g, b] = conversions.okhsv_to_srgb(h, s, v).map((n) => isNaN(n) ? 0 : n)
+    const color = new Color(r, g, b)
 
     // Keeps original parameters
     color.hue = h
@@ -70,10 +40,7 @@ export default class Color {
   transform(matrix) {
     if (matrix.length != 3 || matrix.some(row => row.length != 3)) throw new Error('Invalid matrix size. use 3 by 3')
 
-    const vec = [this.red, this.green, this.blue]
-    const [r, g, b] = matrix.map((row) => {
-      return row.reduce((acc, curr, i) => acc + curr * vec[i], 0)
-    })
+    const [r, g, b] = matMul(this.vec, matrix)
 
     return new Color(r, g, b)
   }
